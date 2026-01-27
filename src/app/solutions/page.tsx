252f472981887bebd7ffe0f598748e9cdb3d81df
Sudhanshu Srivastava <87563365+sudh-202@users.noreply.github.com>
2026-01-27 19:42:49 +0530

    fix: correct component import casing in solutions page

--- src/app/solutions/page.tsx
+++ src/app/solutions/page.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { SolutionsBanner } from "../../components/solutions/SolutioinsBanner";
 import { SolutionsAISpeed } from "../../components/solutions/SolutioinsAISpeed";
-import { SolutionsTemporaryStaffing } from "../../components/solutions/solutionsTemporaryStaffing";
-import { SolutionsPermanentPlacement } from "../../components/solutions/solutionsPermanentPlacement";
-import { SolutionsContractToHire } from "../../components/solutions/solutionsContractToHire";
-import { SolutionsExecutiveSearch } from "../../components/solutions/solutionsExecutiveSearch";
-import { SolutionsIncludes } from "../../components/solutions/solutionsIncludes";
-import { SolutionsBuiltFor } from "../../components/solutions/solutionsBuiltFor";
+import { SolutionsTemporaryStaffing } from "../../components/solutions/SolutionsTemporaryStaffing";
+import { SolutionsPermanentPlacement } from "../../components/solutions/SolutionsPermanentPlacement";
+import { SolutionsContractToHire } from "../../components/solutions/SolutionsContractToHire";
+import { SolutionsExecutiveSearch } from "../../components/solutions/SolutionsExecutiveSearch";
+import { SolutionsIncludes } from "../../components/solutions/SolutionsIncludes";
+import { SolutionsBuiltFor } from "../../components/solutions/SolutionsBuiltFor";
 
 export default function Solutions() {
   return (
